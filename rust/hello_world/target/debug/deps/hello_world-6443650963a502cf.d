9b74375f2b5a324b21aebc651c047b6653d38310
s-haya-123 <ganbaru.wakenai@gmail.com>
2019-10-27 15:47:38 +0900

    learn rust https://doc.rust-lang.org/book/ch02-00-guessing-game-tutorial.html

---
+++ rust/hello_world/target/debug/deps/hello_world-6443650963a502cf.d
@@ -0,0 +1,5 @@
+/Users/hayamimakototarou/Documents/rust/hello_world/target/debug/deps/hello_world-6443650963a502cf: src/main.rs
+
+/Users/hayamimakototarou/Documents/rust/hello_world/target/debug/deps/hello_world-6443650963a502cf.d: src/main.rs
+
+src/main.rs:
---
+++ rust/hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/Users/hayamimakototarou/Documents/rust/hello_world/target/debug/hello_world: /Users/hayamimakototarou/Documents/rust/hello_world/src/main.rs
